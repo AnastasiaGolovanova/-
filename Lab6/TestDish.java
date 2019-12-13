42f12d22721e16eef32f9216028c138dced74069
AnastasiaGolovanova <56160785+AnastasiaGolovanova@users.noreply.github.com>
2019-12-14 00:56:22 +0300

    Add files via upload

---
+++ Lab6/TestDish.java
@@ -0,0 +1,9 @@
+package ru.mirea.lab6;
+
+public class TestDish {
+    public static void main(String[] args) {
+        Tarelka t = new Tarelka("Фарфоровая","Синий","Маленькая");
+        System.out.println(t);
+    }
+}
+
